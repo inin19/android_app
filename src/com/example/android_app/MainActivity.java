35e4be6ff2c371100f7940a28650be34b1fb5ca5
inin <cyy870119@gmail.com>
2014-03-03 00:30:13 -0500

    added Activity

--- src/com/example/android_app/MainActivity.java
+++ src/com/example/android_app/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.android_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "com.example.android.MESSAGE";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,15 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    public void sendMessage(View view){
+    	Intent intent = new Intent(this,DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
     
 }
